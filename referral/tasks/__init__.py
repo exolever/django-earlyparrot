0fb6e729d1639a7013a9a889e525b58bc7d384b8
marfyl <marfyl.dev@gmail.com>
2019-09-04 13:06:40 +0200

    Clean code

--- referral/tasks/__init__.py
+++ referral/tasks/__init__.py
@@ -1,5 +1,5 @@
 from celery import current_app as app
 
-from .campaign import CampaignSubscribeTask  # noqa
+from .campaign import CampaignSubscribeTask
 
 app.tasks.register(CampaignSubscribeTask())
