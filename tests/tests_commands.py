07023a35a9a0ba93d3e622061967d67640b68411
Javier Sújar <javier.sujar@gmail.com>
2019-10-25 14:51:02 +0200

    Fix duplicate subscribers

--- tests/tests_commands.py
+++ tests/tests_commands.py
@@ -48,3 +48,19 @@ class CommandsTestCase(TestCase):
         # ASSERTS
         for user in users:
             self.assertIn(user, campaign.users.all())
+
+        self.assertEqual(campaign.users.count(), 3)
+
+    def test_command_add_users_to_campaign_to_not_duplicate_users(self):
+        # PREPARE DATA
+        campaign = FakeCampaignFactory.create()
+        filepath = './tests/campaign_users.csv'
+        users = self.create_users_from_csv(filepath)
+
+        # DO ACTION
+        with patch('referral.tasks.subscriber.SubscriberGetTokenTask.apply_async') as patch_task:
+            call_command('add_users_to_campaign', '-c', campaign.campaign_id, '-f', filepath)
+            call_command('add_users_to_campaign', '-c', campaign.campaign_id, '-f', filepath)
+
+        # ASSERTS
+        self.assertEqual(campaign.users.count(), 3)
